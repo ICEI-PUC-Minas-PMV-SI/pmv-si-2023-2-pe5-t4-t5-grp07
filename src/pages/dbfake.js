2b7ebe4f2b95fa8738b5bbfe6fa6c86c8b48542a
Vitor Sales <vitorluzdesales@gmail.com>
2023-12-07 12:56:56 -0300

    db produtos

---
+++ src/pages/dbfake.js
@@ -0,0 +1,186 @@
+var db = {
+    dados: [
+
+        {
+            codigo: '0001',
+            descricao: 'Ovos Branco Perfa 20 Un',
+            categoria: 'carnes',
+            imagem: '../img/ovo.webp',
+            preco: [
+                {
+                supermercado1: '11.98',
+                supermercado2: '12.50',
+                supermercado3: '10.30'
+                }
+            ]
+
+        },
+
+        {
+            codigo: '0002',
+            descricao: 'Café Torrado E Moído Extraforte Fino Grão Pacote 500g',
+            categoria: 'cafe',
+            imagem: '../img/cafe.jpg',
+            preco: [
+                {
+                supermercado1: '13.98',
+                supermercado2: '11.50',
+                supermercado3: '10.00'
+                }
+            ]
+
+        },
+
+        {
+            codigo: '0003',
+            descricao: 'Salgadinho Doritos Queijo Nacho 140g',
+            categoria: 'petiscos',
+            imagem: '../img/doritos.jpg',
+            preco: [
+                {
+                supermercado1: '9.98',
+                supermercado2: '8.50',
+                supermercado3: '10.50'
+                }
+            ]
+
+        },
+
+        {
+            codigo: '0004',
+            descricao: 'Ovos Branco Perfa 20 Un',
+            categoria: 'carnes',
+            imagem: '../img/ovo.webp',
+            preco: [
+                {
+                supermercado1: '11.98',
+                supermercado2: '12.50',
+                supermercado3: '10.30'
+                }
+            ]
+
+        },
+
+        {
+            codigo: '0005',
+            descricao: 'Ovos Branco Perfa 20 Un',
+            categoria: 'carnes',
+            imagem: '../img/ovo.webp',
+            preco: [
+                {
+                supermercado1: '11.98',
+                supermercado2: '12.50',
+                supermercado3: '10.30'
+                }
+            ]
+
+        },
+
+        {
+            codigo: '0006',
+            descricao: 'Ovos Branco Perfa 20 Un',
+            categoria: 'carnes',
+            imagem: '../img/ovo.webp',
+            preco: [
+                {
+                supermercado1: '11.98',
+                supermercado2: '12.50',
+                supermercado3: '10.30'
+                }
+            ]
+
+        },
+
+        {
+            codigo: '0007',
+            descricao: 'Ovos Branco Perfa 20 Un',
+            categoria: 'carnes',
+            imagem: '../img/ovo.webp',
+            preco: [
+                {
+                supermercado1: '11.98',
+                supermercado2: '12.50',
+                supermercado3: '10.30'
+                }
+            ]
+
+        },
+
+        {
+            codigo: '0008',
+            descricao: 'Ovos Branco Perfa 20 Un',
+            categoria: 'carnes',
+            imagem: '../img/ovo.webp',
+            preco: [
+                {
+                supermercado1: '11.98',
+                supermercado2: '12.50',
+                supermercado3: '10.30'
+                }
+            ]
+
+        },
+
+        {
+            codigo: '0009',
+            descricao: 'Ovos Branco Perfa 20 Un',
+            categoria: 'carnes',
+            imagem: '../img/ovo.webp',
+            preco: [
+                {
+                supermercado1: '11.98',
+                supermercado2: '12.50',
+                supermercado3: '10.30'
+                }
+            ]
+
+        },
+
+        {
+            codigo: '0010',
+            descricao: 'Ovos Branco Perfa 20 Un',
+            categoria: 'carnes',
+            imagem: '../img/ovo.webp',
+            preco: [
+                {
+                supermercado1: '11.98',
+                supermercado2: '12.50',
+                supermercado3: '10.30'
+                }
+            ]
+
+        },
+
+        {
+            codigo: '0011',
+            descricao: 'Ovos Branco Perfa 20 Un',
+            categoria: 'carnes',
+            imagem: '../img/ovo.webp',
+            preco: [
+                {
+                supermercado1: '11.98',
+                supermercado2: '12.50',
+                supermercado3: '10.30'
+                }
+            ]
+
+        },
+
+        {
+            codigo: '0012',
+            descricao: 'Ovos Branco Perfa 20 Un',
+            categoria: 'carnes',
+            imagem: '../img/ovo.webp',
+            preco: [
+                {
+                supermercado1: '11.98',
+                supermercado2: '12.50',
+                supermercado3: '10.30'
+                }
+            ]
+
+        },
+
+    ]
+}
+
