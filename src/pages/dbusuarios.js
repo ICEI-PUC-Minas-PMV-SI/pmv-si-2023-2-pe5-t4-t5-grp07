663ad0c8a56e95d7918e0a9559d091466b5aed84
Vitor Sales <vitorluzdesales@gmail.com>
2023-12-07 22:34:03 -0300

    login

---
+++ src/pages/dbusuarios.js
@@ -0,0 +1,35 @@
+var db = {
+
+    usuarios: [
+        {
+            login: 'admin@compare.com',
+            senha: '123456'
+        },
+
+        {
+            login: 'vitor@compare.com',
+            senha: '123456'
+        },
+
+        {
+            login: 'guilherme@compare.com',
+            senha: '123456'
+        },
+
+        {
+            login: 'carlos@compare.com',
+            senha: '123456'
+        },
+
+        {
+            login: 'cristiano@compare.com',
+            senha: '123456'
+        }
+
+
+    ]
+
+}
+
+
+
